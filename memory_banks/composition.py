fixed normalization logic issues

memory_banks/composition.py
@@ -105,7 +105,8 @@ class CompositionMemoryBank(nn.Module):
         for i in range(len(self.memory_bank)):
             test_vec = self.memory_bank[i].unsqueeze(0)
             distances = torch.cdist(test_vec, self.memory_bank, p=2.0)
-            min_dist = distances.min().item()
+            sorted_dists, _ = distances.sort(dim=1)
+            min_dist = sorted_dists[0, 1].item()
             if min_dist > max_dist:
                 max_dist = min_dist
 
@@ -114,7 +115,7 @@ class CompositionMemoryBank(nn.Module):
     def score(self,
               features: torch.Tensor,
               mask: torch.Tensor
-              ) -> Tuple[torch.Tensor,float]:
+              ) -> Tuple[float,float]:
         #features should be [1, C, H, W] and mask should be [1,H,W]
         #ensure correct dimensions first
         if features.dim() == 3:
@@ -134,4 +135,4 @@ class CompositionMemoryBank(nn.Module):
         normalized_score = raw_distance / self.max_train_distance.item()
         normalized_score = min(normalized_score, 1.0) #clamp to [0,1]
 
-        return torch.tensor(raw_distance), normalized_score
+        return raw_distance, normalized_score

memory_banks/patchcore.py
@@ -190,7 +190,7 @@ class PatchMemoryBank(nn.Module):
     def score(self,
               test_embeddings: torch.Tensor,
               feature_map_shape: Tuple[int,int]
-              )-> Tuple[torch.Tensor, float]:
+              )-> Tuple[torch.Tensor, float, float]:
         #computes the path-level anomaly scores and then aggregates them
         device = test_embeddings.device
 
@@ -214,5 +214,6 @@ class PatchMemoryBank(nn.Module):
         anomaly_map_smoothed = self.blur(anomaly_map_upscaled)
 
         anomaly_score = torch.max(min_distances).item()
+        anomaly_score_normalized = (anomaly_score - self.mean) / self.std
 
-        return anomaly_map_smoothed.squeeze().cpu(), anomaly_score
+        return anomaly_map_smoothed.squeeze().cpu(), anomaly_score, anomaly_score_normalized